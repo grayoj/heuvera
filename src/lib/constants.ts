55dfc641b4a2af67be3f541a9ec94634fbf07c77
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2025-03-19 14:13:50 +0100

    feat: added constants for project

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 const HEUVERA_API_KEY = process.env.HEUVERA_API_KEY;
+const PLUNK_API_URL = 'https://api.useplunk.com/v1/transactional/send';
 
-export { HEUVERA_API_KEY };
+export { HEUVERA_API_KEY, PLUNK_API_URL };
