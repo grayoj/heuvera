e86b10f238d9c1cd07765ba1df5945ba89f54430
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2025-03-20 16:25:07 +0100

    fix: fix marketplace pages

--- src/app/marketplace/explore/page.tsx
+++ src/app/marketplace/explore/page.tsx
@@ -7,38 +7,38 @@ import MarketplaceLayout from "../page";
 import { motion } from "framer-motion";
 
 export default function Explore() {
-    return (
-        <>
-            <MarketplaceLayout>
-                <div className="flex flex-col flex-1 h-full w-full">
-                    <Categories />
-                    <motion.div
-                        className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-x-10 gap-y-10 justify-items-stretch"
-                        initial="hidden"
-                        animate="visible"
-                        variants={{
-                            hidden: { opacity: 0 },
-                            visible: {
-                                opacity: 1,
-                                transition: { staggerChildren: 0.15 }, // Stagger delay between items
-                            },
-                        }}
-                    >
-                        {PropertyData.map((property) => (
-                            <motion.div
-                                key={property.id}
-                                variants={{
-                                    hidden: { opacity: 0, y: 30 }, // Start hidden and slightly lower
-                                    visible: { opacity: 1, y: 0 }, // Fade in and move up
-                                }}
-                                transition={{ duration: 0.6, ease: "easeOut" }} // Smooth transition
-                            >
-                                <PropertyCard property={property} />
-                            </motion.div>
-                        ))}
-                    </motion.div>
-                </div>
-            </MarketplaceLayout>
-        </>
-    );
+  return (
+    <>
+      <MarketplaceLayout>
+        <div className="flex flex-col flex-1 h-full w-full">
+          <Categories />
+          <motion.div
+            className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-x-10 gap-y-10 justify-items-stretch"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: { opacity: 0 },
+              visible: {
+                opacity: 1,
+                transition: { staggerChildren: 0.15 }, // Stagger delay between items
+              },
+            }}
+          >
+            {PropertyData.map((property) => (
+              <motion.div
+                key={property.id}
+                variants={{
+                  hidden: { opacity: 0, y: 30 }, // Start hidden and slightly lower
+                  visible: { opacity: 1, y: 0 }, // Fade in and move up
+                }}
+                transition={{ duration: 0.6, ease: "easeOut" }} // Smooth transition
+              >
+                <PropertyCard property={property} />
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </MarketplaceLayout>
+    </>
+  );
 }
--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -5,55 +5,55 @@ import { LucideBell, LucideSearch } from "lucide-react";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import { HeuveraLogo } from "@heuvera/app/components/logo";
 
-export default function MarketplaceLayout({ children }: { children: React.ReactNode, }) {
-    const [selected, setSelected] = useState<string>("Explore");
-    const NavigationContent = useMemo(() => [
-        { title: "Explore", link: "/marketplace/explore" },
-        { title: "Favorites", link: "#favorites" },
-        { title: "Discover", link: "#discover" },
-        { title: "Contact", link: "#contact" },
-    ], []);
+export default function MarketplaceLayout({ children }: { children: React.ReactNode }) {
+  const [selected, setSelected] = useState<string>("Explore");
+  const NavigationContent = useMemo(() => [
+    { title: "Explore", link: "/marketplace/explore" },
+    { title: "Favorites", link: "#favorites" },
+    { title: "Discover", link: "#discover" },
+    { title: "Contact", link: "#contact" },
+  ], []);
 
-    return (
-        <>
-            <div className="w-full h-full bg-[#F3F2EC] px-20 flex flex-col">
-                <div className="h-24 w-full flex items-center justify-between">
-                    <div className="w-52">
-                        <HeuveraLogo width={35} height={35} />
-                    </div>
-                    <div className="hidden md:flex items-center space-x-12 md:space-x-4 lg:space-x-6 xl:space-x-10 2xl:space-x-12">
-                        {NavigationContent.map((content, index) => (
-                            <div key={index}>
-                                <button
-                                    onClick={() => setSelected(content.title)}
-                                    className={`text-base md:text-xs lg:text-xs xl:text-base 2xl:text-base font-medium font-serif transition-colors duration-300 px-2 pb-2 ${selected === content.title
-                                        ? "text-[#7B4F3A] font-semibold border-[#7B4F3A] border-b-2"
-                                        : "text-[#323232] hover:text-primary"
-                                        }`}
-                                >
-                                    {content.title}
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="w-52 flex flex-row items-center justify-between">
-                        <LucideSearch className="text-[#323232] text-2xl" />
-                        <div className="flex gap-2 items-center">
-                            <div className="h-6 w-6 rounded-full">
-                                <Avatar className="rounded-full overflow-hidden block">
-                                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                    <AvatarFallback className="bg-[#E3E2D9] font-serif">FG</AvatarFallback>
-                                </Avatar>
-                            </div>
-                            <h1 className="text-base font-medium font-serif text-[#323232]">George</h1>
-                        </div>
-                        <LucideBell className="text-2xl text-[#323232]" />
-                    </div>
-                </div>
-                <div className="pb-10 w-full flex-1 flex">
-                    {children}
-                </div>
+  return (
+    <>
+      <div className="w-full h-full bg-[#F3F2EC] px-20 flex flex-col">
+        <div className="h-24 w-full flex items-center justify-between">
+          <div className="w-52">
+            <HeuveraLogo width={35} height={35} />
+          </div>
+          <div className="hidden md:flex items-center space-x-12 md:space-x-4 lg:space-x-6 xl:space-x-10 2xl:space-x-12">
+            {NavigationContent.map((content, index) => (
+              <div key={index}>
+                <button
+                  onClick={() => setSelected(content.title)}
+                  className={`text-base md:text-xs lg:text-xs xl:text-base 2xl:text-base font-medium font-serif transition-colors duration-300 px-2 pb-2 ${selected === content.title
+                    ? "text-[#7B4F3A] font-semibold border-[#7B4F3A] border-b-2"
+                    : "text-[#323232] hover:text-primary"
+                    }`}
+                >
+                  {content.title}
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="w-52 flex flex-row items-center justify-between">
+            <LucideSearch className="text-[#323232] text-2xl" />
+            <div className="flex gap-2 items-center">
+              <div className="h-6 w-6 rounded-full">
+                <Avatar className="rounded-full overflow-hidden block">
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback className="bg-[#E3E2D9] font-serif">FG</AvatarFallback>
+                </Avatar>
+              </div>
+              <h1 className="text-base font-medium font-serif text-[#323232]">George</h1>
             </div>
-        </>
-    )
+            <LucideBell className="text-2xl text-[#323232]" />
+          </div>
+        </div>
+        <div className="pb-10 w-full flex-1 flex">
+          {children}
+        </div>
+      </div>
+    </>
+  )
 }
