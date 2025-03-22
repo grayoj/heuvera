ecf92e220d75311a32382f6e68431a4f846ea79d
ftgeorge <ftgeorgean@gmail.com>
2025-03-22 01:21:32 +0100

    feat: to map link added

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-
 import FilterButton from "./FilterButton";
 import LeaseRentStays from "./LeaseRentStays";
 import CategoryList from "./CategoryList";
@@ -9,10 +8,17 @@ import useIsMobile from "@heuvera/hooks/IsMobile";
 import MapButton from "./MapButton";
 
 const Categories = () => {
+    const router = useRouter();
+
+    // Function to navigate to /explore/map
+    const handleMapNavigation = () => {
+        router.push("/explore/map");
+    };
+
     return (
         <div className="w-full border-t border-b border-[#E3E2D9] h-[88px] flex flex-row items-center">
             {/* Map Button */}
-            <MapButton onClick={}/>
+            <MapButton onClick={handleMapNavigation} />
 
             {/* Lease, Rent, Stays Section */}
             <div className="flex items-center w-full justify-evenly">
