fc1ee57a0499036869778adfaace53f9c44be188
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2025-04-07 19:08:06 +0100

    Updated layout and pages route for account

---
+++ src/app/(main)/account/layout.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@heuvera/components/ui/button";
+import SidebarItem from "@heuvera/components/ui/SidebarItem";
+import { sideBar } from "@heuvera/app/data/array";
+import useIsMobile from "@heuvera/hooks/IsMobile";
+import MobileProfileSettings from "@heuvera/components/modal/MobileSidebarModal";
+
+interface Item {
+  icon: React.ComponentType<{ className?: string }>;
+  name: string;
+  description: string;
+  route: string;
+}
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return <MobileProfileSettings />;
+  }
+
+  return (
+    <div className="flex h-screen">
+      <aside className="w-[25%] md:w-[30%] lg:w-[25%] xl:w-[25%] 2xl:w-[25%] bg-[#F8F7F2] dark:bg-[#333333] border-r-[1px] flex flex-col gap-2 overflow-y-auto">
+        <div className="w-full px-4 md:px-8 lg:px-12 xl:px-14 2xl:px-20 py-4">
+          <Button variant="outline" onClick={() => router.push("/")} className="cursor-pointer">
+            <ArrowLeft className="mr-2" /> Back to Home
+          </Button>
+        </div>
+        <div className="w-full">
+          {sideBar.map((item: Item) => (
+            <div key={item.route}>
+              <SidebarItem item={item} route={item.route}>
+                <div className="flex items-center">
+                  <div className="flex flex-col ml-3">
+                    <h2 className="font-medium text-sm">{item.name}</h2>
+                    <p className="text-[#898989] text-xs w-full md:w-auto lg:w-[8.5rem] xl:w-[10rem] 2xl:w-[12.5rem] line-clamp-2 overflow-hidden text-ellipsis">
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
+              </SidebarItem>
+            </div>
+          ))}
+        </div>
+      </aside>
+
+      <main className="flex-1 min-h-0 overflow-y-auto pl-4 pr-4 md:pr-8 lg:pr-12 xl:pr-14 2xl:pr-20 pb-4 bg-[#F8F7F2] dark:bg-[#333333]">
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ src/app/(main)/account/support/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import AccountHeader from "@heuvera/components/ui/AccountHeader";
+import Help from "@heuvera/components/ui/Help";
+import Input from "@heuvera/components/ui/Input";
+import ProfileBanner from "@heuvera/components/ui/ProfileBanner";
+import Textarea from "@heuvera/components/ui/Textarea";
+import { Button } from "@heuvera/components/ui/button";
+import { useState } from "react";
+
+export default function Support() {
+  const [isHelpVisible, setIsHelpVisible] = useState(false);
+
+  const toggleHelp = () => {
+    setIsHelpVisible(!isHelpVisible);
+  };
+  return (
+    <>
+      <ProfileBanner toggleHelp={toggleHelp} isHelpVisible={isHelpVisible} />
+      <AccountHeader
+        heading="Support & Assistance"
+        subheading="Get help, contact support, and access resources to enhance your experience."
+      />
+
+      <div className="flex flex-col md:flex-col lg:flex-row justify-between my-5 gap-5 md:gap-5 font-serif">
+        <div className="flex-grow">
+          <h2 className="text-[1.25rem] font-normal mb-[.94rem]">
+            Submit an inquiry
+          </h2>
+          <form className="flex flex-col space-y-4 h-full w-full">
+            <div className="flex flex-col space-y-4">
+              <Input label="Full name" className="w-full" />
+              <Input label="Email" className="w-full" />
+            </div>
+
+            <Textarea
+              label="Issue Description"
+              initialValue="Hello, I need help with..."
+            />
+            <Button
+              variant="default"
+              className="bg-[#7B4F3A] dark:bg-[#8B5F4D] text-white py-[0.6875rem] px-[2.5rem] hover:bg-[#664130] hover:cursor-pointer"
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
+        <Help isVisible={isHelpVisible} />
+      </div>
+    </>
+  );
+}
