9e5a54364ac05edd48925f0334ea017fcf9fafd5
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2025-04-07 19:03:49 +0100

    custom authentication hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,17 @@
+'use client'; 
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { getSession } from '@auth0/nextjs-auth0';
+
+export function useAuthClient() {
+  const { user, error, isLoading } = useUser();
+  return { user, error, isLoading, isAuthenticated: !!user };
+}
+
+export async function useAuthServer() {
+  const session = await getSession();
+  const user = session?.user;
+  return {
+    user,
+    isAuthenticated: !!user,
+  };
+}
